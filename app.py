4caee63c28e36e3c2a29224de8339be524c980f3
João Paulo Alves Ferreira <iamferreirajp@gmail.com>
2017-12-05 18:14:53 -0200

    Setting up CORS

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_cors import CORS
 
 from security import authenticate, identity
 from resources.user import User, UserAccess, UserList
@@ -11,6 +12,8 @@ from resources.transport import Transport, TransportList, TransportCreate
 from resources.box import Box, BoxList
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'EiEiO'
